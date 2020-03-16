4789f9c9bdcda452530fe7fa341aaec14d400563
Hartmut Schorrig <hartmut@vishia.org>
2020-03-16 21:12:38 +0100

    commit of compile vishiaBase-2020-03-14.jar

--- +gradleBuild.bat
+++ +gradleBuild.bat
@@ -1,11 +1,10 @@
-set JAVA_HOME=c:\Programs\Java\jdk1.8.0_211
-::if exist T:\vishia\Testbuild rmdir /S/Q T:\vishia\Testbuild
-if not exist T:\vishia\Testbuild mkdir T:\vishia\Testbuild
-if not exist build mklink /J build T:\vishia\Testbuild
-if exist build\reports rmdir /S/Q build\reports
-::pause
-call gradle.bat test 2>build\err.txt
+if "%JAVA_HOME%" == "" set JAVA_HOME=c:\Programs\Java\jdk1.8.0_211
+call +mkLinkBuildGradle.bat
+:repeat
+cls
+call gradle.bat --warning-mode all test 2>build\err.txt
 type build\err.txt
 pause
+goto :repeat
 call inetBrowser %CD%\build\reports\tests\test\index.html
 
--- src/test/java/org/vishia/util/test/TestStringCmp.java
+++ src/test/java/org/vishia/util/test/TestStringCmp.java
@@ -14,8 +14,8 @@ public class TestStringCmp {
   public void testSkipComments() {
     String s1 = "abc (*xxx*) zz";
     String s2 = "abc zz";
-    boolean bOk = StringCmp.compare(s1, s2, true, this.comments);
-    Assert.assertTrue("fails: (*comment*) ", bOk);
+    int errorpos = StringCmp.compare(s1, s2, true, this.comments);
+    Assert.assertTrue("fails: (*comment*) ", errorpos==-1);
   }
   
   
@@ -25,13 +25,13 @@ public class TestStringCmp {
   public void testSkipEndlineComment() {
     String s1 = "abc // zz\r\n";
     String s2 = "abc";
-    boolean bOk = true; 
-    bOk = StringCmp.compare(s1, s2, true, this.comments);
-    Assert.assertTrue("fails: abc //comment", bOk);;
+    int errorpos; 
+    errorpos = StringCmp.compare(s1, s2, true, this.comments);
+    Assert.assertTrue("fails: abc //comment", errorpos == -1);;
     s1 = "abc// zz\r\n";
     s2 = "abc";
-    bOk = StringCmp.compare(s1, s2, true, this.comments);
-    Assert.assertTrue("fails: abc//comment", bOk);;
+    errorpos = StringCmp.compare(s1, s2, true, this.comments);
+    Assert.assertTrue("fails: abc//comment", errorpos == -1);;
   }
   
   
