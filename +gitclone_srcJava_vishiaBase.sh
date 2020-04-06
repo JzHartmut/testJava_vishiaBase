a5f1fcc61f089424dca7209afdbaf5fb86b6fe9c
Hartmut Schorrig <hartmut@vishia.org>
2020-04-07 00:22:13 +0200

    tag of srcJava

--- +gitclone_srcJava_vishiaBase.sh
+++ +gitclone_srcJava_vishiaBase.sh
@@ -2,7 +2,7 @@ echo this shell script gets the srcJava_vishiaBase core sources
 echo if not exists src/main/java/org: clone https://github.com/JzHartmut/srcJava_vishiaBase.git
 cd `dirname $0`  ##script directory as current
 if ! test -d src/main/java/org; then
-  git clone -b 2020-03-24 https://github.com/JzHartmut/srcJava_vishiaBase.git src/main/java
+  git clone -b 2020-04-04 https://github.com/JzHartmut/srcJava_vishiaBase.git src/main/java
   cd src/main/java
   pwd
   echo touch all files with the timestamp in .filelist:
